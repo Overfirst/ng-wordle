35a8680cfdca8d0ec4b2c4953e79e027fc83702d
Overfirst <overfirst@inbox.ru>
2024-05-04 13:28:19 +0300

    Заготовки стейта и логики нажатия кнопок

---
+++ src/app/wordle/services/wordle-state.service.ts
@@ -0,0 +1,85 @@
+import { Injectable, OnDestroy } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { WordleWord } from '../model/wordle.model';
+
+@Injectable()
+export class WordleStateService implements OnDestroy {
+  /* Количество попыток */
+  public readonly attemptsCount: number = 6;
+
+  /* Длина слова по умолчанию */
+  public readonly defaultWordLength: number = 5;
+
+  /* Минимальная длина слова */
+  public readonly minWordLength: number = 4;
+
+  /* Максимальная длина слова */
+  public readonly maxWordLength: number = 11;
+
+  /* Длина загаданного слова */
+  private readonly wordLength$$: BehaviorSubject<number> =
+    new BehaviorSubject<number>(5);
+
+  public readonly wordLength$: Observable<number> =
+    this.wordLength$$.asObservable();
+
+  public get wordLength(): number {
+    return this.wordLength$$.getValue();
+  }
+
+  public set wordLength(length: number) {
+    this.wordLength$$.next(length);
+  }
+
+  /* Загаданное слово */
+  private readonly secretWord$$: BehaviorSubject<string> =
+    new BehaviorSubject<string>('абвгд');
+
+  public readonly secretWord$: Observable<string> =
+    this.secretWord$$.asObservable();
+
+  public get secretWord(): string {
+    return this.secretWord$$.getValue();
+  }
+
+  public set secretWord(word: string) {
+    this.secretWord$$.next(word);
+  }
+
+  /* Вводимое слово */
+  private readonly inputWord$$: BehaviorSubject<WordleWord> =
+    new BehaviorSubject<WordleWord>([]);
+
+  public readonly inputWord$: Observable<WordleWord> =
+    this.inputWord$$.asObservable();
+
+  public get inputWord(): WordleWord {
+    return this.inputWord$$.getValue();
+  }
+
+  public set inputWord(word: WordleWord) {
+    this.inputWord$$.next(word);
+  }
+
+  /* Введённые слова */
+  private readonly wordRows$$: BehaviorSubject<WordleWord[]> =
+    new BehaviorSubject<WordleWord[]>([]);
+
+  public readonly wordRows$: Observable<WordleWord[]> =
+    this.wordRows$$.asObservable();
+
+  public get wordRows(): WordleWord[] {
+    return this.wordRows$$.getValue();
+  }
+
+  public set wordRows(rows: WordleWord[]) {
+    this.wordRows$$.next(rows);
+  }
+
+  public ngOnDestroy(): void {
+    this.wordLength$$.complete();
+    this.secretWord$$.complete();
+    this.inputWord$$.complete();
+    this.wordRows$$.complete();
+  }
+}
---
+++ src/app/wordle/services/wordle.service.ts
@@ -0,0 +1,93 @@
+import { Inject, Injectable } from '@angular/core';
+import { filter, fromEvent, map, merge, Observable, of, tap } from 'rxjs';
+import { DOCUMENT } from '@angular/common';
+import { WordleStateService } from './wordle-state.service';
+import { WordleLetter } from '../model/wordle.model';
+
+@Injectable()
+export class WordleService {
+  constructor(
+    @Inject(DOCUMENT)
+    private readonly document: Document,
+    private readonly wordleState: WordleStateService,
+  ) {
+    this.initSubscriptions();
+  }
+
+  private initSubscriptions(): void {
+    merge(this.listenKeyboard(), this.listenWordLengthChanges()).subscribe();
+  }
+
+  private listenKeyboard(): Observable<unknown> {
+    return fromEvent<KeyboardEvent>(this.document, 'keydown').pipe(
+      map((event: KeyboardEvent) => event.key),
+
+      filter((key: string) => {
+        const isActionKey: boolean = ['Backspace', 'Enter'].includes(key);
+        const isLetterKey: boolean = /^[а-яА-Я]+$/.test(key);
+
+        return isActionKey || isLetterKey;
+      }),
+
+      tap((key: string) => this.handleKey(key)),
+    );
+  }
+
+  private listenWordLengthChanges(): Observable<unknown> {
+    return of(null);
+  }
+
+  private handleKey(key: string): void {
+    switch (key) {
+      case 'Backspace':
+        this.handleBackspace();
+        break;
+
+      case 'Enter':
+        this.handleEnter();
+        break;
+
+      default:
+        this.handleLetterKey(key);
+    }
+  }
+
+  private handleBackspace(): void {
+    /* Удаляем последнюю букву */
+    if (this.wordleState.inputWord.length > 0) {
+      this.wordleState.inputWord = this.wordleState.inputWord.slice(0, -1);
+    }
+  }
+
+  private handleEnter(): void {
+    const inputEqualSecretLength: boolean =
+      this.wordleState.inputWord.length === this.wordleState.secretWord.length;
+
+    const rowsLengthAllowed: boolean =
+      this.wordleState.wordRows.length < this.wordleState.attemptsCount - 1;
+
+    if (inputEqualSecretLength && rowsLengthAllowed) {
+      this.wordleState.wordRows = [
+        ...this.wordleState.wordRows,
+        this.wordleState.inputWord,
+      ];
+
+      this.wordleState.inputWord = [];
+    }
+  }
+
+  private handleLetterKey(key: string): void {
+    /* Добавляем новую букву */
+    if (
+      this.wordleState.inputWord.length < this.wordleState.secretWord.length
+    ) {
+      const newLetter: WordleLetter = {
+        letter: key,
+        index: this.wordleState.inputWord.length,
+        type: 'unknown',
+      };
+
+      this.wordleState.inputWord = [...this.wordleState.inputWord, newLetter];
+    }
+  }
+}
