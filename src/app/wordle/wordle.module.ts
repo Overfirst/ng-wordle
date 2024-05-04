7f26213cae738ffcf77ba6cf732911e85469f1ec
Overfirst <overfirst@inbox.ru>
2024-05-04 13:29:12 +0300

    Пайп для вывода букв

--- src/app/wordle/wordle.module.ts
+++ src/app/wordle/wordle.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WordleComponent } from './wordle.component';
+import { WordleExtractLetterPipe } from './pipes/wordle-extract-letter.pipe';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [WordleComponent],
+  declarations: [WordleComponent, WordleExtractLetterPipe],
   exports: [WordleComponent],
 })
 export class WordleModule {}
