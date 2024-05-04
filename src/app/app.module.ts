adcfb6e8d12dea091a97249ab7cc9cffb7d80240
Overfirst <overfirst@inbox.ru>
2024-05-04 13:23:16 +0300

    Вывод Wordle в AppComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WordleModule } from './wordle/wordle.module';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
   declarations: [AppComponent],
+  imports: [BrowserModule, WordleModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
